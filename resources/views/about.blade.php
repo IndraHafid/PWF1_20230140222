e0216fb8a634d3f4ad014d5e6ea1e750379096c6
IndraHfid <indrahafidsaputra@gmail.com>
2026-02-28 09:29:08 +0700

    Pertemuan-2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                <p><strong>Nama:</strong> Indra Hafid Saputra</p>
+                <p><strong>NIM:</strong> 20230140222</p>
+                <p><strong>Program Studi:</strong> Teknologi Informasi</p>
+                <p><strong>Hobi:</strong> Apa Aja</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
